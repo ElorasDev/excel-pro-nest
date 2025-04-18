c9dbe8a95e618257f9254efd80331f3c4a3faa67
Aryan Salemabadi <aryanavatar1380@gmail.com>
2025-04-18 23:43:07 +0330

    some change

--- src/modules/sms/sms.controller.ts
+++ src/modules/sms/sms.controller.ts
@@ -1,7 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { SmsService } from './sms.service';
 
 @Controller('sms')
 export class SmsController {
-  constructor(private readonly smsService: SmsService) {}
+  constructor() {}
 }
