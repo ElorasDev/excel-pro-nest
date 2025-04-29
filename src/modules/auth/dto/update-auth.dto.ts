f63654f6933ab8a14ba21e6a960714c35ca81dcb
Aryan Salemabadi <aryanavatar1380@gmail.com>
2025-04-30 00:48:29 +0330

    some change on admin module

--- src/modules/auth/dto/update-auth.dto.ts
+++ src/modules/auth/dto/update-auth.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAuthDto } from './create-auth.dto';
+import { IsString, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
+export class UpdateAuthDto extends PartialType(CreateAuthDto) {
+  @ApiProperty({
+    description: 'Current password for validation',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  currentPassword?: string;
+}
